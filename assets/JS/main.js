a7fbc1a8846f4c8351b321341f1528081da1f92a
AlmirGabriel <almir.gabriel.andrade@gmail.com>
2024-04-17 00:49:34 -0300

    'New Changes on CSS and function'

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,16 +1,31 @@
 function criaCalculadora() {
   return {
-    display: document.querySelector(".diplay"),
+    display: document.querySelector('.display'),
+    btnClear: document.querySelector('.btn-clear'),
+
+    clearDisplay() {
+      this.display.value = '';
+    },
+    apagaUm() {
+      this.display.value = this.display.value.slice(0, -1);
+    },
 
     inicia() {
       this.cliqueBotoes();
     },
 
     cliqueBotoes() {
-      document.addEventListener("click", function (e) {
+      document.addEventListener('click', e => {
         const el = e.target;
-        if (el.classList.contains("btn-num")) {
+
+        if (el.classList.contains('btn-num')) {
           this.btnParaDisplay(el.innerText);
+        }
+        if(el.classList.contains('btn-clear')) {
+          this.clearDisplay();
+        }
+        if(el.classList.contains('btn-del')) {
+          this.apagaUm();
         }
       });
     },
